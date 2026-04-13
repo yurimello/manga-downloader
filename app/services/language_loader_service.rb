962d1af0f92ba169f11fc072c0e6146c29ee683e
Yuri Mello <yuri.mcr@gmail.com>
2026-04-13 17:32:01 -0300

    Centralize language loading in LanguageLoaderService
    - LanguageLoaderService.call returns sorted language codes
    - LanguageLoaderService.priorities returns code-to-priority map
    - ChapterSelectorService uses LanguageLoaderService.priorities as default
    - No class reads languages.yml directly anymore

--- app/services/language_loader_service.rb
+++ app/services/language_loader_service.rb
@@ -1,6 +1,15 @@
 class LanguageLoaderService
   def self.call
-    config = YAML.load_file(Rails.root.join("config", "languages.yml"))
     config["languages"].sort_by { |l| l["priority"] }.map { |l| l["code"] }
+  end
+
+  def self.priorities
+    config["languages"].each_with_object({}) do |lang, hash|
+      hash[lang["code"]] = lang["priority"]
+    end
+  end
+
+  def self.config
+    @config ||= YAML.load_file(Rails.root.join("config", "languages.yml"))
   end
 end
